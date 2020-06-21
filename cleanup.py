6be10eb8c774fc5c0c41cda49bd1f74163121b33
Annie Lydens <annie@thoughtbot.com>
2020-06-20 20:52:39 -0500

    Add in topic nodes to vis graphs
    Add df output below graphs

--- cleanup.py
+++ cleanup.py
@@ -2,6 +2,9 @@
 
 import json
 
+def clean_label(label):
+    return label.replace('http://www.cidoc-crm.org/cidoc-crm/', '').replace('http://www.ics.forth.gr/isl/CRMsci/', '').replace('http://www.ics.forth.gr/isl/CRMba/', '')
+
 def main():
     with open('full_topic_nodes.json') as f:
         data = json.load(f)
@@ -13,11 +16,20 @@ def main():
     cleaned_edges = []
     
     for edge in data['edges']:
-        if str(edge['source']) in node_ids or str(edge['target']) in node_ids:
+        if (str(edge['source']) in node_ids) and (str(edge['target']) in node_ids):
+            edge['title'] = clean_label(edge['label'])
+            del edge['label']
             cleaned_edges.append(edge)
 
-    print(json.dumps(cleaned_edges))
+    # print(json.dumps(cleaned_edges))
+    # print(len(cleaned_edges))
+    cleaned_nodes = []
+    for node in data['nodes']:
+        node['label'] = clean_label(node['label'])
+        node['title'] = clean_label(node['label'])
+        cleaned_nodes.append(node)
 
+    print(json.dumps(cleaned_nodes))
 
 if __name__ == '__main__':
     main()
---
+++ node_lookup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import json
+import re
+
+def main():
+    with open('topic_nodes.json') as f:
+        data = json.load(f)
+
+    nodes = data['nodes']
+    nodes_dict = {}
+    for node in nodes:
+        nodes_dict[node['id']] = node['label']
+
+    print(json.dumps(nodes_dict))
+
+if __name__ == '__main__':
+    main()
